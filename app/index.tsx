d833ccb17ea8cd10bcdb541d8ef80ee4849b43e1
hun <pursuit0819@naver.com>
2025-03-11 01:58:05 +0900

    api 호출 연습

--- app/index.tsx
+++ app/index.tsx
@@ -6,11 +6,14 @@ import {
   Dimensions,
   Button,
   Linking,
+  ActivityIndicator,
 } from 'react-native';
 import * as Location from 'expo-location';
 import Header from './components/header';
 import Footer from './components/footer';
 import { useEffect, useState } from 'react';
+// import { WeekperWeather } from './apis/weather';
+import { locationInfo } from './types/location';
 
 type cityInfoType = {
   city: string | null;
@@ -22,7 +25,88 @@ const cityInfo: cityInfoType = {
   district: '...Loading',
 };
 
+type listType = {
+  dt: number;
+  main: {
+    temp: number;
+    feels_like: number;
+    temp_min: number;
+    temp_max: number;
+    pressure: number;
+    sea_level: number;
+    grnd_level: number;
+    humidity: number;
+    temp_kf: number;
+  };
+  weather: [
+    {
+      id: number;
+      main: string;
+      description: string;
+      icon: string;
+    }
+  ];
+  clouds: {
+    all: number;
+  };
+  wind: {
+    speed: number;
+    deg: number;
+    gust: number;
+  };
+  visibility: number;
+  pop: number;
+  rain: {
+    [key: string]: number;
+  };
+  sys: {
+    [key: string]: string;
+  };
+  dt_txt: string;
+};
+
+// const defaultList = {
+//   dt: 1661871600,
+//   main: {
+//     temp: 296.76,
+//     feels_like: 296.98,
+//     temp_min: 296.76,
+//     temp_max: 297.87,
+//     pressure: 1015,
+//     sea_level: 1015,
+//     grnd_level: 933,
+//     humidity: 69,
+//     temp_kf: -1.11,
+//   },
+//   weather: [
+//     {
+//       id: 500,
+//       main: 'Rain',
+//       description: 'light rain',
+//       icon: '10d',
+//     },
+//   ],
+//   clouds: {
+//     all: 100,
+//   },
+//   wind: {
+//     speed: 0.62,
+//     deg: 349,
+//     gust: 1.18,
+//   },
+//   visibility: 10000,
+//   pop: 0.32,
+//   rain: {
+//     '3h': 0.26,
+//   },
+//   sys: {
+//     pod: 'd',
+//   },
+//   dt_txt: '2022-08-30 15:00:00',
+// };
+
 const { width: SCREEN_WIDTH } = Dimensions.get('window');
+const WEATHER_API_KEY = 'api key';
 
 const styles = StyleSheet.create({
   container: {
@@ -65,8 +149,9 @@ const styles = StyleSheet.create({
 
 const App = () => {
   const [city, setCity] = useState<cityInfoType | null>(cityInfo);
-  const [location, setLocation] = useState<Location.LocationObject | null>();
+  const [location, setLocation] = useState(locationInfo);
   const [ok, setOk] = useState<boolean>(false);
+  const [lists, setLists] = useState<listType[]>([]);
 
   const ask = async () => {
     await permissionLocation();
@@ -75,12 +160,33 @@ const App = () => {
       coords: { latitude, longitude },
     } = await Location.getCurrentPositionAsync({ accuracy: 5 });
 
+    setLocation({ lat: latitude, lng: longitude });
+
     // reverse Geocoding : 지리적 좌표로 설명된 위치를 사람이 읽을 수 있는 주소 또는 장소 이름으로 변환하는 프로세스
     const location = await Location.reverseGeocodeAsync({
       latitude,
       longitude,
     });
+
     setCity({ city: location[0].city, district: location[0].district });
+
+    const request = await fetch(
+      `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${WEATHER_API_KEY}`
+    );
+
+    const response = await request.json();
+    console.log(response);
+
+    const convertKTCList = response.list.filter(
+      ({ dt_txt }: { dt_txt: string }) => {
+        const date = new Date(dt_txt);
+        // KTC
+        date.setHours(date.getHours() + 9);
+
+        return {};
+      }
+    );
+    setLists(convertKTCList);
   };
 
   const permissionLocation = async () => {
@@ -123,22 +229,22 @@ const App = () => {
             horizontal
             contentContainerStyle={styles.weather}
           >
-            <View style={styles.day}>
-              <Text style={styles.temp}>27</Text>
-              <Text style={styles.description}>Sunny</Text>
-            </View>
-            <View style={styles.day}>
-              <Text style={styles.temp}>27</Text>
-              <Text style={styles.description}>Sunny</Text>
-            </View>
-            <View style={styles.day}>
-              <Text style={styles.temp}>27</Text>
-              <Text style={styles.description}>Sunny</Text>
-            </View>
-            <View style={styles.day}>
-              <Text style={styles.temp}>27</Text>
-              <Text style={styles.description}>Sunny</Text>
-            </View>
+            {lists.length === 0 ? (
+              <View style={styles.day}>
+                <ActivityIndicator color="white" size="large" />
+              </View>
+            ) : (
+              lists.map((list) => (
+                <View style={styles.day}>
+                  <Text style={styles.temp}>
+                    {Number(list.main.temp - 273).toFixed(1)}
+                  </Text>
+                  <Text style={styles.description}>
+                    {list.weather[0].description}
+                  </Text>
+                </View>
+              ))
+            )}
           </ScrollView>
         </View>
         <Footer />
---
+++ app/types/location.ts
@@ -0,0 +1,9 @@
+export type LocationType = {
+  lat: number | null;
+  lng: number | null;
+};
+
+export const locationInfo: LocationType = {
+  lat: 0,
+  lng: 0,
+};
